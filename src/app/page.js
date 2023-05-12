6d623193f8829b819c3b09e1812ee817feea9eab
Maher Nassrallah <maher.ple2@gmail.com>
2023-05-12 16:21:24 +0300

    small changes to solve freecodecamp problem

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,51 @@ import { Fragment, useEffect, useState } from "react";
 
 export default function Home() {
   const [html, setHtml] = useState();
-  const [textareaInput, setTextareaInput] = useState();
+  const [textareaInput, setTextareaInput] = useState(`
+  # Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+
+  Heres some code, \`<div></div>\`, between 2 backticks.
+
+  \`\`\`
+  // this is multi-line code:
+
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\\\`\\\`\\\`' && lastLine == '\\\`\\\`\\\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+
+
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+
+  ![freeCodeCamp Logo](https://cdn.discordapp.com/attachments/1106113737968070677/1106571466549502042/image.png)`);
+  
   const [editorFullScreen, setEditorFullScreen] = useState(false);
   const [previewerFullScreen, setPreviewFullScreen] = useState(false);
 
@@ -14,6 +58,11 @@ export default function Home() {
     let inputToHtml = marked(event.target.value);
     setHtml(inputToHtml);
   };
+
+  useEffect(() => {
+    let inputToHtml = marked(textareaInput);
+    setHtml(inputToHtml);
+  }, []);
 
   const editorFullScreenHandler = () => {
     setEditorFullScreen(!editorFullScreen);
